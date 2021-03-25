a6c49d2bb18f168af5ba23e4216296897788f74f
Piotr Rogowski <piotr.rogowski0@gmail.com>
2021-03-25 22:31:50 +0100

    Base fields

--- src/components/Log.tsx
+++ src/components/Log.tsx
@@ -6,15 +6,18 @@ import {
   Spin,
   Layout,
   Tabs,
-  Space,
+  Checkbox,
+  Row,
+  Skeleton,
 } from 'antd';
 import {
   FileTextOutlined,
   EditOutlined,
   DashboardOutlined,
 } from '@ant-design/icons';
+import { CheckboxValueType } from 'antd/lib/checkbox/Group';
 import { Parser } from 'mlg-converter';
-import { Result as ParserResult } from 'mlg-converter/dist/types';
+import { Field, Result as ParserResult } from 'mlg-converter/dist/types';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import { loadLogs } from '../utils/api';
 import Canvas, { LogEntry } from './Log/Canvas';
@@ -35,6 +38,14 @@ const Log = () => {
   } as any;
   const [isLoading, setIsLoading] = useState(true);
   const [logs, setLogs] = useState<ParserResult>();
+  const [fields, setFields] = useState<Field[]>([]);
+  const [selectedFields, setSelectedFields] = useState<CheckboxValueType[]>([
+    'RPM',
+    'TPS',
+    'AFR Target',
+    'AFR',
+    'MAP',
+  ]);
 
   useEffect(() => {
     loadLogs()
@@ -43,28 +54,40 @@ const Log = () => {
         const parsed = new Parser(data).parse();
         setLogs(parsed);
         setIsLoading(false);
-        console.log(parsed);
+        setFields(parsed.fields);
 
-        // plot(parsed.records as any);
+        console.log(parsed);
       });
   }, []);
 
   return (
     <>
       <Sider {...siderProps} className="app-sidebar">
-        <Tabs defaultActiveKey="files" style={{ marginLeft: 20 }}>
-          <TabPane tab={<FileTextOutlined />} key="files">
-            <PerfectScrollbar options={{ suppressScrollX: true }}>
-              Content of Tab Pane 1
-            </PerfectScrollbar>
-          </TabPane>
-          <TabPane tab={<DashboardOutlined />} key="gauges">
-            Content of Tab Pane 2
-          </TabPane>
-          <TabPane tab={<EditOutlined />} key="fields">
-            Content of Tab Pane 3
-          </TabPane>
-        </Tabs>
+        {isLoading ?
+          <Skeleton />
+          :
+            <Tabs defaultActiveKey="fields" style={{ marginLeft: 20 }}>
+            <TabPane tab={<EditOutlined />} key="fields">
+              <PerfectScrollbar options={{ suppressScrollX: true }}>
+                <Checkbox.Group onChange={setSelectedFields} value={selectedFields}>
+                  {fields.map((field) => (
+                    <Row key={field.name}>
+                      <Checkbox key={field.name} value={field.name}>{field.name}</Checkbox>
+                    </Row>
+                  ))}
+                </Checkbox.Group>
+              </PerfectScrollbar>
+            </TabPane>
+            <TabPane tab={<FileTextOutlined />} key="files">
+              <PerfectScrollbar options={{ suppressScrollX: true }}>
+                Files
+              </PerfectScrollbar>
+            </TabPane>
+            <TabPane tab={<DashboardOutlined />} key="gauges">
+              Gauges
+            </TabPane>
+          </Tabs>
+        }
       </Sider>
       <Layout style={{ width: '100%', textAlign: 'center', marginTop: 50 }}>
         <Content>
