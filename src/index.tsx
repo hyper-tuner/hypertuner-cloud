15bb623a1688c83589d65d678b7ded09fecf0ec4
Piotr Rogowski <piotr.rogowski0@gmail.com>
2021-05-02 23:18:23 +0200

    Add Sentry (#73)
    * Update dependencies
    
    * Add Sentry
    
    * Removed unused vars

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,24 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import App from './App';
 import store from './store';
+import {
+  environment,
+  isProduction,
+  sentryDsn,
+} from './utils/env';
+
+if (isProduction) {
+  Sentry.init({
+    dsn: sentryDsn,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 0.2,
+    environment,
+  });
+}
 
 ReactDOM.render(
   <HashRouter>
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,4 +1,7 @@
 export const isWeb = process.env.APP_PLATFORM === 'web';
 export const isDesktop = process.env.APP_PLATFORM === 'desktop';
 export const isMac = `${window.navigator.platform}`.includes('Mac');
+export const environment = process.env.NODE_ENV || 'development';
+export const isProduction = environment === 'production';
+export const sentryDsn = process.env.REACT_APP_SENTRY_DSN;
 export const platform = `${window.navigator.platform}`;
