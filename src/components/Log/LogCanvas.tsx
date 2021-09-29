98931a55bfb46f2fd302f1916df4b88c8124c231
Piotr Rogowski <piotr.rogowski0@gmail.com>
2021-09-29 23:04:04 +0200

    Move thing around

--- src/components/Log/LogCanvas.tsx
+++ src/components/Log/LogCanvas.tsx
@@ -52,14 +52,56 @@ const LogCanvas = ({ data, width, height, selectedFields }: Props) => {
   const svgRef = useRef<SVGSVGElement | null>(null);
   const [zoomState, setZoomState] = useState<ZoomTransform | null>(null);
   const [lines, setLines] = useState<string[]>([]);
-  const [fieldsToPlot, setFieldsToPlot] = useState<{ [index: string]: PlottableField } | null>(null);
+  // const [fieldsToPlot, setFieldsToPlot] = useState<{ [index: string]: PlottableField } | null>(null);
 
   const hsl = useCallback((fieldIndex: number, allFields: number) => {
     const [hue] = colorHsl(0, allFields - 1, fieldIndex);
     return `hsl(${hue}, 90%, 50%)`;
   }, []);
 
+  const fieldsOnly = (entry: LogEntry) => entry.type === 'field';
+
+  const filtered = useMemo(() => data.filter(fieldsOnly), [data]);
+
+  // find max values for each selected field so we can calculate scale
+  const fieldsToPlot = useMemo(() => {
+    console.log('calc!');
+
+
+    const temp: { [index: string]: PlottableField } = {};
+
+    filtered.forEach((entry) => {
+      selectedFields.forEach(({ label, scale, transform, units, format }, index) => {
+        const value = entry[label];
+
+        if (!temp[label]) {
+          temp[label] = {
+            min: 0,
+            max: 0,
+            scale: (scale || 1) as number,
+            transform: (transform || 0) as number,
+            units: units || '',
+            format: format || '',
+            color: hsl(index, MAX_FIELDS),
+          };
+        }
+
+        if (value > temp[label].max) {
+          temp[label].max = entry[label] as number;
+        }
+
+        if (value < temp[label].min) {
+          temp[label].min = entry[label] as number;
+        }
+      });
+    });
+
+    return temp;
+  }, [filtered, hsl, selectedFields]);
+
   useEffect(() => {
+    console.log('render :(');
+
     const xValue = (entry: LogEntry): number => (entry.Time || 0) as number;
     const yValue = (entry: LogEntry, field: SelectedField): number => {
       if (!(field.label in entry)) {
@@ -71,7 +113,7 @@ const LogCanvas = ({ data, width, height, selectedFields }: Props) => {
     };
 
     const xScale = scaleLinear()
-      .domain([0, max(data, xValue) as number])
+      .domain([0, max(filtered, xValue) as number])
       .range([0, width]);
 
     if (zoomState) {
@@ -79,45 +121,9 @@ const LogCanvas = ({ data, width, height, selectedFields }: Props) => {
       xScale.domain(newXScale.domain());
     }
 
-    const fieldsOnly = (entry: LogEntry) => entry.type === 'field';
-
-    const filtered = data.filter(fieldsOnly);
-
-    // find max values for each selected field so we can calculate scale
-    setFieldsToPlot(() => {
-      const temp: { [index: string]: PlottableField } = {};
-
-      filtered.forEach((entry) => {
-        selectedFields.forEach(({ label, scale, transform, units, format }, index) => {
-          const value = entry[label];
-
-          if (!temp[label]) {
-            temp[label] = {
-              min: 0,
-              max: 0,
-              scale: (scale || 1) as number,
-              transform: (transform || 0) as number,
-              units: units || '',
-              format: format || '',
-              color: hsl(index, MAX_FIELDS),
-            };
-          }
-
-          if (value > temp[label].max) {
-            temp[label].max = entry[label] as number;
-          }
-
-          if (value < temp[label].min) {
-            temp[label].min = entry[label] as number;
-          }
-        });
-      });
-
-      return temp;
-    });
-
+    // TODO: this does not have to be recomputed on every zoom level change
     const linesRaw = () => selectedFields.map((field) => {
-      const yField = (fieldsToPlot || {})[field.label] || { min:0, max: 0 };
+      const yField = (fieldsToPlot || {})[field.label] || { min: 0, max: 0 };
 
       const yScale = scaleLinear()
         .domain([yField.min, yField.max])
@@ -132,9 +138,7 @@ const LogCanvas = ({ data, width, height, selectedFields }: Props) => {
 
     const svg = select(svgRef.current);
 
-    const zoomed = () => {
-      setZoomState(zoomTransform(svg.node() as any));
-    };
+    const zoomed = () => setZoomState(zoomTransform(svg.node() as any));
 
     const zoomBehavior = zoom()
       .scaleExtent([1, 1000])  // zoom boundaries
@@ -143,17 +147,18 @@ const LogCanvas = ({ data, width, height, selectedFields }: Props) => {
       .on('zoom', zoomed);
 
     svg.call(zoomBehavior as any);
-  }, [data, fieldsToPlot, height, hsl, selectedFields, width, zoomState]);
+  }, [data, fieldsToPlot, filtered, height, hsl, selectedFields, width, zoomState]);
 
   return (
     <svg width={width} height={height} ref={svgRef}>
       <g>
-        {data.length && fieldsToPlot ? lines.map((field, index) => (
+        {data.length ? lines.map((field, index) => (
           <path
             key={selectedFields[index].name}
             d={field}
             fill="none"
-            stroke={(fieldsToPlot[selectedFields[index].label] || {}).color}
+            // stroke={(fieldsToPlot[selectedFields[index].label] || {}).color}
+            stroke="red"
             strokeWidth={2}
           />)) : null}
       </g>
