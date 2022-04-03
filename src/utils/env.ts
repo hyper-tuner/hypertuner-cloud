fe91066c76d8328d41651435308398e4a0edaa81
Piotr Rogowski <piotr.rogowski0@gmail.com>
2022-04-03 23:59:44 +0200

    Fix env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,5 +1,5 @@
 export const isMac = `${window.navigator.platform}`.includes('Mac');
-export const environment = import.meta.env.NODE_ENV || 'development';
+export const environment = import.meta.env.VITE_ENVIRONMENT || 'development';
 export const isProduction = environment === 'production';
 export const sentryDsn = import.meta.env.VITE_SENTRY_DSN;
 export const platform = `${window.navigator.platform}`;
