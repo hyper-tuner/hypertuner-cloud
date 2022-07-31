892181e0c5927adafc9fd643f996d3c83d45daaf
Piotr Rogowski <piotr.rogowski0@gmail.com>
2022-07-31 19:37:56 +0200

    Add GTM

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
+import ReactGA from 'react-ga4';
 import App from './App';
 import store from './store';
 import {
@@ -20,6 +21,11 @@ if (isProduction) {
     tracesSampleRate: 0.2,
     environment: environment as string,
   });
+
+  if (import.meta.env.VITE_GTM_ID) {
+    ReactGA.initialize(import.meta.env.VITE_GTM_ID);
+    ReactGA.send({ hitType: 'pageview', page: window.location.hash });
+  }
 }
 
 const container = document.getElementById('root');
