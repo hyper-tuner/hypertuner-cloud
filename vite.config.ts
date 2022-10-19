80885b07b7fe964c1b3b56d40e6ac945479aaa03
Piotr Rogowski <piotr.rogowski0@gmail.com>
2022-10-20 00:03:43 +0200

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -35,5 +36,49 @@ export default defineConfig({
   plugins: [
     react(),
     visualizer(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['logo.ico', 'logo.png'],
+      manifest: {
+        name: 'HyperTuner Cloud',
+        short_name: 'HyperTuner',
+        description: 'HyperTuner Cloud - Share your tunes and logs',
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#191C1E',
+        background_color: '#191C1E',
+        icons: [
+          {
+            src: '/icons/icon.ico',
+            type: 'image/x-icon',
+            sizes: '256x256',
+          },
+          {
+            src: '/icons/icon.png',
+            type: 'image/png',
+            sizes: '512x512',
+          },
+        ],
+        screenshots: [
+          {
+            src: '/img/screen2.png',
+            sizes: '1920x1194',
+            type: 'image/png',
+            platform: 'wide',
+            label: 'Log viewer',
+          },
+          {
+            src: '/img/screen1.png',
+            sizes: '1920x1194',
+            type: 'image/png',
+            platform: 'wide',
+            label: 'VE Table with command palette',
+          },
+        ],
+      },
+    }),
   ],
 });
