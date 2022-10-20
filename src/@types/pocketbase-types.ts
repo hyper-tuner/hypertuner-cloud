c7dbc7d382f1c2dc879eb5e70172821ef798a08b
Piotr Rogowski <piotr.rogowski0@gmail.com>
2022-10-20 11:35:09 +0200

    Bump pocketbase-typegen

--- src/@types/pocketbase-types.ts
+++ src/@types/pocketbase-types.ts
@@ -1,46 +1,52 @@
-// Generated using pocketbase-typegen
+// This file was @generated using pocketbase-typegen
 
 export enum Collections {
+	IniFiles = 'iniFiles',
 	Profiles = 'profiles',
 	Tunes = 'tunes',
-	IniFiles = 'iniFiles',
-}
-
-export type ProfilesRecord = {
-	userId: string;
-	username: string;
-	avatar?: string;
-}
-
-export type TunesRecord = {
-	user: string;
-	userProfile: string;
-	tuneId: string;
-	signature: string;
-	vehicleName: string;
-	engineMake: string;
-	engineCode: string;
-	displacement: number;
-	cylindersCount: number;
-	aspiration: string;
-	compression?: number;
-	fuel?: string;
-	ignition?: string;
-	injectorsSize?: number;
-	year?: number;
-	hp?: number;
-	stockHp?: number;
-	readme: string;
-	textSearch: string;
-	visibility: string;
-	tuneFile: string;
-	customIniFile?: string;
-	logFiles?: string[];
-	toothLogFiles?: string[];
 }
 
 export type IniFilesRecord = {
-	signature: string;
-	file: string;
-	ecosystem: string;
+	signature: string
+	file: string
+	ecosystem: string
+}
+
+export type ProfilesRecord = {
+	userId: string
+	username: string
+	avatar?: string
+}
+
+export type TunesRecord = {
+	user: string
+	userProfile: string
+	tuneId: string
+	signature: string
+	vehicleName: string
+	engineMake: string
+	engineCode: string
+	displacement: number
+	cylindersCount: number
+	aspiration: string
+	compression?: number
+	fuel?: string
+	ignition?: string
+	injectorsSize?: number
+	year?: number
+	hp?: number
+	stockHp?: number
+	readme: string
+	textSearch: string
+	visibility: string
+	tuneFile: string
+	customIniFile?: string
+	logFiles?: string[]
+	toothLogFiles?: string[]
+}
+
+export type CollectionRecords = {
+	iniFiles: IniFilesRecord
+	profiles: ProfilesRecord
+	tunes: TunesRecord
 }
