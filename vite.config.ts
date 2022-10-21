bafc1b6d488045acf02d96c6728c4ada234a4bcb
Piotr Rogowski <piotr.rogowski0@gmail.com>
2022-10-21 10:14:02 +0200

    Remove PWA (#826)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -37,49 +36,5 @@ export default defineConfig({
   plugins: [
     react(),
     visualizer(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
-      },
-      includeAssets: ['icons/icon.ico', 'icons/icon.png'],
-      manifest: {
-        name: 'HyperTuner Cloud',
-        short_name: 'HyperTuner Cloud',
-        description: 'HyperTuner Cloud - Share your tunes and logs',
-        start_url: '.',
-        display: 'standalone',
-        theme_color: '#191C1E',
-        background_color: '#191C1E',
-        icons: [
-          {
-            src: '/icons/icon.ico',
-            type: 'image/x-icon',
-            sizes: '256x256',
-          },
-          {
-            src: '/icons/icon.png',
-            type: 'image/png',
-            sizes: '512x512',
-          },
-        ],
-        screenshots: [
-          {
-            src: '/img/screen2.png',
-            sizes: '1920x1194',
-            type: 'image/png',
-            platform: 'wide',
-            label: 'Log viewer',
-          },
-          {
-            src: '/img/screen1.png',
-            sizes: '1920x1194',
-            type: 'image/png',
-            platform: 'wide',
-            label: 'VE Table with command palette',
-          },
-        ],
-      },
-    }),
   ],
 });
