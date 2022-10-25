f717e8766e8772520969752a63c6f9b8a587d642
Piotr Rogowski <piotr.rogowski0@gmail.com>
2022-10-25 20:20:47 +0200

    Add pagination for tooth and composite logs

---
+++ src/components/TriggerLogs/LogsPagination.tsx
@@ -0,0 +1,41 @@
+import { Pagination } from 'antd';
+import {
+  ReactNode,
+  useState,
+} from 'react';
+
+interface LogsPaginationProps {
+  children: ReactNode;
+  total: number;
+  pageSize: number;
+  onChange: (indexFrom: number, indexTo: number) => void;
+}
+
+const LogsPagination = (props: LogsPaginationProps) => {
+  const [page, setPage] = useState(1);
+  const { children, total, pageSize, onChange } = props;
+
+  const changePage = (newPage: number) => {
+    setPage(newPage);
+    const indexFrom = (newPage - 1) * pageSize;
+    const indexTo = indexFrom + pageSize;
+    onChange(indexFrom, indexTo);
+  };
+
+  return (
+    <div style={{ position: 'relative' }}>
+      <div style={{ position: 'absolute', bottom: 0, right: 70, zIndex: 1 }}>
+        <Pagination
+          simple
+          defaultPageSize={pageSize}
+          current={page}
+          total={total}
+          onChange={changePage}
+        />
+      </div>
+      {children}
+    </div >
+  );
+};
+
+export default LogsPagination;
