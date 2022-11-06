3c9476b73a3f39b649bc2924628b9573bc195181
Piotr Rogowski <piotr.rogowski0@gmail.com>
2022-11-06 10:55:56 +0100

    Regenerate types

--- src/@types/pocketbase-types.ts
+++ src/@types/pocketbase-types.ts
@@ -7,58 +7,67 @@ export type RecordIdString = string
 export type UserIdString = string
 
 export type BaseRecord = {
-	id: RecordIdString
-	created: IsoDateString
-	updated: IsoDateString
-	collectionId: string
-	collectionName: string
-	expand?: { [key: string]: any }
+  id: RecordIdString
+  created: IsoDateString
+  updated: IsoDateString
+  collectionId: string
+  collectionName: string
+  expand?: { [key: string]: any }
 }
 
 export enum Collections {
-	IniFiles = 'iniFiles',
-	Tunes = 'tunes',
-	Users = 'users',
+  IniFiles = 'iniFiles',
+  Stargazers = 'stargazers',
+  Tunes = 'tunes',
+  Users = 'users',
 }
 
 export type IniFilesRecord = {
-	signature: string
-	file: string
-	ecosystem: 'speeduino' | 'rusefi'
+  signature: string
+  file: string
+  ecosystem: 'speeduino' | 'rusefi'
 }
 
 export type IniFilesResponse = IniFilesRecord & BaseRecord
 
+export type StargazersRecord = {
+  user: RecordIdString
+  tune: RecordIdString
+}
+
+export type StargazersResponse = StargazersRecord & BaseRecord
+
 export type TunesRecord = {
-	author: RecordIdString
-	tuneId: string
-	signature: string
-	vehicleName: string
-	engineMake: string
-	engineCode: string
-	displacement: number
-	cylindersCount: number
-	aspiration: 'na' | 'turbocharged' | 'supercharged'
-	compression?: number
-	fuel?: string
-	ignition?: string
-	injectorsSize?: number
-	year?: number
-	hp?: number
-	stockHp?: number
-	readme: string
-	textSearch: string
-	visibility: 'public' | 'unlisted'
-	tuneFile: string
-	customIniFile?: string
-	logFiles?: string[]
-	toothLogFiles?: string[]
+  author: RecordIdString
+  tuneId: string
+  signature: string
+  stars?: number
+  vehicleName: string
+  engineMake: string
+  engineCode: string
+  displacement: number
+  cylindersCount: number
+  aspiration: 'na' | 'turbocharged' | 'supercharged'
+  compression?: number
+  fuel?: string
+  ignition?: string
+  injectorsSize?: number
+  year?: number
+  hp?: number
+  stockHp?: number
+  readme: string
+  textSearch: string
+  visibility: 'public' | 'unlisted'
+  tuneFile: string
+  customIniFile?: string
+  logFiles?: string[]
+  toothLogFiles?: string[]
 }
 
 export type TunesResponse = TunesRecord & BaseRecord
 
 export type UsersRecord = {
-	avatar?: string
+  avatar?: string
   username: string
   email: string
   verified: boolean
@@ -67,7 +76,8 @@ export type UsersRecord = {
 export type UsersResponse = UsersRecord & BaseRecord
 
 export type CollectionRecords = {
-	iniFiles: IniFilesRecord
-	tunes: TunesRecord
-	users: UsersRecord
+  iniFiles: IniFilesRecord
+  stargazers: StargazersRecord
+  tunes: TunesRecord
+  users: UsersRecord
 }
