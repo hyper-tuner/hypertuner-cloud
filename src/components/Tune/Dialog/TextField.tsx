e59cebf10a4e31fc77d9d1d572090b9185ad055a
Piotr Rogowski <piotr.rogowski0@gmail.com>
2023-06-02 12:30:04 +0200

    Make https addresses in text fields render as links

--- src/components/Tune/Dialog/TextField.tsx
+++ src/components/Tune/Dialog/TextField.tsx
@@ -6,16 +6,33 @@ const TextField = ({ title }: { title: string }) => {
     '!': 'warning',
   };
   const type = types[title.charAt(0)];
+  const message = type ? title.substring(1) : title;
+  let messageTag = <span>message</span>;
+
+  // check if message contains a link and render it as a link
+  const linkRegex = /https?:\/\/[^\s]+/g;
+  const linkMatch = message.match(linkRegex);
+  if (linkMatch) {
+    const link = linkMatch[0];
+    const linkIndex = message.indexOf(link);
+    const beforeLink = message.substring(0, linkIndex);
+    const afterLink = message.substring(linkIndex + link.length);
+
+    messageTag = (
+      <>
+        {beforeLink}
+        <a href={link} target='_blank' rel='noreferrer' style={{ color: 'inherit' }}>
+          {link}
+        </a>
+        {afterLink}
+      </>
+    );
+  }
 
   return (
     <Typography.Paragraph style={{ display: 'flex', justifyContent: 'center' }}>
       {type ? (
-        <Alert
-          message={type ? title.substring(1) : title}
-          type={type}
-          showIcon
-          style={{ width: '100%', maxWidth: 700 }}
-        />
+        <Alert message={messageTag} type={type} showIcon style={{ width: '100%', maxWidth: 700 }} />
       ) : (
         <Typography.Text type='secondary'>{title}</Typography.Text>
       )}
